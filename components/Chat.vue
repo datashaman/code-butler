7a7e3c1e61ff40965c758199bd6378883e3c4ed5
Marlin Forbes <marlinf@datashaman.com>
2024-09-24 18:06:33 +0200

    Manual adjustments made to Chat.vue to add left padding to the runs container.

--- components/Chat.vue
+++ components/Chat.vue
@@ -408,7 +408,7 @@ onMounted(async () => {
         </div>
       </div>
       <div
-        class="flex-1 flex flex-col gap-2 overflow-y-auto"
+        class="flex-1 flex flex-col gap-2 pl-4 overflow-y-auto"
         ref="runsContainer"
       >
         <Run
