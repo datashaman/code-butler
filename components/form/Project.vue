53fefbe58b9c0668ed2c3139b2b5a4fd35658ddf
Marlin Forbes <marlinf@datashaman.com>
2024-09-24 14:32:41 +0200

    fix facts UI on project form

--- components/form/Project.vue
+++ components/form/Project.vue
@@ -15,7 +15,7 @@ const name = ref("")
 const path = ref("")
 const assistantId = ref("")
 const description = ref("")
-const facts = ref("")
+const facts = ref([])
 
 const errors = ref({})
 
@@ -108,78 +108,100 @@ const deleteProject = async () => {
 }
 </script>
 <template>
-  <div class="w-full lg:w-1/3">
-    <form @submit.prevent="saveProject">
-      <div class="form-control w-full">
-        <div class="label">
-          <div class="label-text">Name</div>
+  <div class="flex flex-row gap-8">
+    <div class="w-full lg:w-1/3">
+      <form @submit.prevent="saveProject">
+        <div class="form-control w-full">
+          <div class="label">
+            <div class="label-text">Name</div>
+          </div>
+          <input type="text" class="input input-bordered" v-model="name" />
+          <div v-if="errors.name" class="label">
+            <div class="label-text-alt text-error">{{ errors.name }}</div>
+          </div>
         </div>
-        <input type="text" class="input input-bordered" v-model="name" />
-        <div v-if="errors.name" class="label">
-          <div class="label-text-alt text-error">{{ errors.name }}</div>
-        </div>
-      </div>
 
-      <div class="form-control w-full">
-        <div class="label">
-          <div class="label-text">Path</div>
+        <div class="form-control w-full">
+          <div class="label">
+            <div class="label-text">Path</div>
+          </div>
+          <input type="text" class="input input-bordered" v-model="path" />
+          <div v-if="errors.path" class="label">
+            <div class="label-text-alt text-error">{{ errors.path }}</div>
+          </div>
         </div>
-        <input type="text" class="input input-bordered" v-model="path" />
-        <div v-if="errors.path" class="label">
-          <div class="label-text-alt text-error">{{ errors.path }}</div>
-        </div>
-      </div>
 
-      <div class="form-control w-full">
-        <div class="label">
-          <div class="label-text">Assistant</div>
+        <div class="form-control w-full">
+          <div class="label">
+            <div class="label-text">Assistant</div>
+          </div>
+          <select class="select select-bordered" v-model="assistantId">
+            <option value="">Select an assistant</option>
+            <option
+              v-for="assistant in assistantStore.assistants"
+              :value="assistant.id"
+            >
+              {{ assistant.name }}
+            </option>
+          </select>
+          <div v-if="errors.assistantId" class="label">
+            <div class="label-text-alt text-error">
+              {{ errors.assistantId }}
+            </div>
+          </div>
         </div>
-        <select class="select select-bordered" v-model="assistantId">
-          <option value="">Select an assistant</option>
-          <option
-            v-for="assistant in assistantStore.assistants"
-            :value="assistant.id"
-          >
-            {{ assistant.name }}
-          </option>
-        </select>
-        <div v-if="errors.assistantId" class="label">
-          <div class="label-text-alt text-error">{{ errors.assistantId }}</div>
-        </div>
-      </div>
 
-      <div class="form-control w-full">
-        <div class="label">
-          <div class="label-text">Description</div>
+        <div class="form-control w-full">
+          <div class="label">
+            <div class="label-text">Description</div>
+          </div>
+          <textarea
+            class="textarea textarea-bordered w-full"
+            v-model="description"
+          />
+          <div v-if="errors.description" class="label">
+            <div class="label-text-alt text-error">
+              {{ errors.description }}
+            </div>
+          </div>
         </div>
-        <textarea
-          class="textarea textarea-bordered w-full"
-          v-model="description"
-        />
-        <div v-if="errors.description" class="label">
-          <div class="label-text-alt text-error">{{ errors.description }}</div>
-        </div>
-      </div>
 
+        <div class="pt-4 w-full flex justify-between">
+          <template v-if="props.projectId">
+            <button class="btn btn-error" @click.prevent="deleteProject">
+              Delete
+            </button>
+            <button class="btn btn-primary" type="submit">Update</button>
+          </template>
+          <button v-else class="btn btn-primary" type="submit">Create</button>
+        </div>
+      </form>
+    </div>
+    <div class="flex-grow">
       <div class="form-control w-full">
         <div class="label">
           <div class="label-text">Facts</div>
         </div>
-        <textarea class="textarea textarea-bordered w-full" v-model="facts" />
-        <div v-if="errors.facts" class="label">
-          <div class="label-text-alt text-error">{{ errors.facts }}</div>
+        <div class="flex flex-col gap-2">
+          <label
+            v-for="(fact, index) in facts"
+            :key="index"
+            class="input input-bordered flex items-center gap-2"
+          >
+            <input type="text" class="grow" v-model="facts[index]" />
+            <v-icon
+              @click="facts.splice(index, 1)"
+              name="hi-x-circle"
+              class="cursor-pointer"
+            >
+              Remove
+            </v-icon>
+          </label>
+          <button @click="facts.push('')" class="btn btn-primary">
+            Add Fact
+          </button>
         </div>
       </div>
-
-      <div class="pt-4 w-full flex justify-between">
-        <template v-if="props.projectId">
-          <button class="btn btn-error" @click.prevent="deleteProject">
-            Delete
-          </button>
-          <button class="btn btn-primary" type="submit">Update</button>
-        </template>
-        <button v-else class="btn btn-primary" type="submit">Create</button>
-      </div>
-    </form>
+    </div>
   </div>
 </template>
--- schemas/projectSchema.ts
+++ schemas/projectSchema.ts
@@ -5,6 +5,6 @@ export const projectSchema = z.object({
   path: z.string().min(1),
   assistantId: z.string().min(1),
   description: z.string().max(1000),
-  facts: z.string().nullable(),
+  facts: z.array(z.string()).optional(),
   threadId: z.string().optional(),
 })
