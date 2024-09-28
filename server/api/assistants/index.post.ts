be9da9d3891b5cfc4512fde7c502a8257b851790
Marlin Forbes <marlinf@datashaman.com>
2024-09-28 11:40:13 +0200

    Remove tools from assistant config, theyre all used at run time

--- server/api/assistants/index.post.ts
+++ server/api/assistants/index.post.ts
@@ -1,13 +1,9 @@
 import { useValidatedBody, z } from "h3-zod"
 import OpenAI from "openai"
-import { useTools } from "~/composables/useTools"
 import { assistantSchema } from "~/schemas/assistantSchema"
 
 export default defineEventHandler(async (event) => {
-  const tools = await useTools({})
   const values = await useValidatedBody(event, assistantSchema)
-
-  values.tools = tools.getTools(values.tools)
 
   const openai = new OpenAI()
   const assistant = await openai.beta.assistants.create(values)
